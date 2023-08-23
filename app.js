f85e667f61663fb5c266b50d591dd7cd1ab79f30
Esquiafo <leocapone@hotmail.com>
2023-08-23 20:06:24 -0300

    feat: mongo example + env

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import runDB from './services/mongoNode.js'
+const app = express();
+
+// Configurar middleware para servir archivos estáticos (CSS, imágenes, etc.)
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send("");
+});
+
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
